d6f4e035b46c3184c374f15cebdd31b88e8a28ab
Jerome Leclanche <jerome@leclan.ch>
2015-09-11 20:00:03 +0200

    Re-add CardType.ABILITY, leave SPELL alias

--- hearthstone/enums.py
+++ hearthstone/enums.py
@@ -291,12 +291,14 @@ class CardType(IntEnum):
 	PLAYER = 2
 	HERO = 3
 	MINION = 4
-	SPELL = 5
+	ABILITY = 5
 	ENCHANTMENT = 6
 	WEAPON = 7
 	ITEM = 8
 	TOKEN = 9
 	HERO_POWER = 10
+
+	SPELL = ABILITY
 
 
 class EnchantmentVisual(IntEnum):
