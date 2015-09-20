e5c24c70100ac712ebbdf5a8f90ef4b5cd916583
Jerome Leclanche <jerome@leclan.ch>
2015-09-20 22:29:16 +0200

    Update PowSubType enum against 9786 PegasusGame.HistoryBlock.Type

--- hearthstone/enums.py
+++ hearthstone/enums.py
@@ -548,14 +548,19 @@ class PowType(IntEnum):
 
 class PowSubType(IntEnum):
 	ATTACK = 1
-	CONTINUOUS = 2
+	JOUST = 2
 	POWER = 3
-	SCRIPT = 4
 	TRIGGER = 5
 	DEATHS = 6
 	PLAY = 7
 	FATIGUE = 8
+
+	# Removed
+	SCRIPT = 4
 	ACTION = 99
+
+	# Renamed
+	CONTINUOUS = 2
 
 
 class State(IntEnum):
