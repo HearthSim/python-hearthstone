01e6e91a8355d0f479b4604625b8eb56a26f70b7
Jerome Leclanche <jerome@leclan.ch>
2015-09-28 00:51:57 +0200

    Add GameType enum (from PegasusShared.GameType)

--- hearthstone/enums.py
+++ hearthstone/enums.py
@@ -499,6 +499,19 @@ class ChoiceType(IntEnum):
 	GENERAL = 2
 
 
+class GameType(IntEnum):
+	GT_UNKNOWN = 0
+	GT_VS_AI = 1
+	GT_VS_FRIEND = 2
+	GT_TUTORIAL = 4
+	GT_ARENA = 5
+	GT_TEST = 6
+	GT_RANKED = 7
+	GT_UNRANKED = 8
+	GT_TAVERNBRAWL = 16
+	GT_LAST = 17
+
+
 class GoldRewardState(IntEnum):
 	INVALID = 0
 	ELIGIBLE = 1
