acb61a9d7a8043ea5ba3eb25498fdb3bcb080a0b
Jerome Leclanche <jerome@leclan.ch>
2016-01-14 16:01:45 +0200

    hslog: Improve entity representation

--- hearthstone/hslog/entities.py
+++ hearthstone/hslog/entities.py
@@ -1,21 +1,28 @@
 class Entity:
+	_args = ()
+
 	def __init__(self, id):
 		self.id = id
 		self.tags = {}
+
+	def __repr__(self):
+		return "%s(id=%r, %s)" % (
+			self.__class__.__name__, self.id,
+			", ".join("%s=%r" % (k, getattr(self, k)) for k in self._args)
+		)
 
 	def tag_change(self, tag, value):
 		self.tags[tag] = value
 
 
 class Game(Entity):
+	_args = ("players", )
+
 	def __init__(self, id):
 		super().__init__(id)
 		self.players = []
 		self.entities = {}
 		self.actions = []
-
-	def __repr__(self):
-		return "%s(players=%r)" % (self.__class__.__name__, self.players)
 
 	def register_entity(self, entity):
 		self.entities[entity.id] = entity
@@ -32,6 +39,8 @@ class Game(Entity):
 
 
 class Player(Entity):
+	_args = ("name", )
+
 	def __init__(self, id, player_id, hi, lo):
 		super().__init__(id)
 		self.player_id = player_id
@@ -39,18 +48,14 @@ class Player(Entity):
 		self.account_lo = lo
 		self.name = None
 
-	def __repr__(self):
-		return "%s(id=%r, name=%r)" % (self.__class__.__name__, self.id, self.name)
-
 
 class Card(Entity):
+	_args = ("card_id", )
+
 	def __init__(self, id, card_id):
 		super().__init__(id)
 		self.card_id = card_id
 		self.revealed = False
-
-	def __repr__(self):
-		return "%s(card_id=%r)" % (self.__class__.__name__, self.card_id)
 
 	def reveal(self, id):
 		self.revealed = True
