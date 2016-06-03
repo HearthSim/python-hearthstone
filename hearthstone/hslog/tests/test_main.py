a09206fe7959bbea8b04552a6a11c9a07e3305fc
Jerome Leclanche <jerome@leclan.ch>
2016-06-03 17:41:35 +0300

    hslog: Add tests for basic game tags initialization

--- hearthstone/hslog/tests/test_main.py
+++ hearthstone/hslog/tests/test_main.py
@@ -1,6 +1,8 @@
 import pytest
 from io import StringIO
-from hearthstone.enums import CardType, GameTag, PowerType, Zone
+from hearthstone.enums import (
+	CardType, GameTag, PlayState, PowerType, State, Step, Zone
+)
 from hearthstone.hslog import LogParser
 
 
@@ -9,6 +11,35 @@ D 02:59:14.6088620 GameState.DebugPrintPower() - CREATE_GAME
 D 02:59:14.6149420 GameState.DebugPrintPower() -     GameEntity EntityID=1
 D 02:59:14.6446530 GameState.DebugPrintPower() -     Player EntityID=2 PlayerID=1 GameAccountId=[hi=1 lo=0]
 D 02:59:14.6481950 GameState.DebugPrintPower() -     Player EntityID=3 PlayerID=2 GameAccountId=[hi=3 lo=2]
+""".strip()
+
+INITIAL_GAME = """
+D 02:59:14.6088620 GameState.DebugPrintPower() - CREATE_GAME
+D 02:59:14.6149420 GameState.DebugPrintPower() -     GameEntity EntityID=1
+D 02:59:14.6420450 GameState.DebugPrintPower() -         tag=TURN value=1
+D 02:59:14.6428100 GameState.DebugPrintPower() -         tag=ZONE value=PLAY
+D 02:59:14.6430430 GameState.DebugPrintPower() -         tag=ENTITY_ID value=1
+D 02:59:14.6436240 GameState.DebugPrintPower() -         tag=NEXT_STEP value=BEGIN_MULLIGAN
+D 02:59:14.6438920 GameState.DebugPrintPower() -         tag=CARDTYPE value=GAME
+D 02:59:14.6442880 GameState.DebugPrintPower() -         tag=STATE value=RUNNING
+D 02:59:14.6446530 GameState.DebugPrintPower() -     Player EntityID=2 PlayerID=1 GameAccountId=[hi=1 lo=0]
+D 02:59:14.6450220 GameState.DebugPrintPower() -         tag=PLAYSTATE value=PLAYING
+D 02:59:14.6463220 GameState.DebugPrintPower() -         tag=PLAYER_ID value=1
+D 02:59:14.6466060 GameState.DebugPrintPower() -         tag=TEAM_ID value=1
+D 02:59:14.6469080 GameState.DebugPrintPower() -         tag=ZONE value=PLAY
+D 02:59:14.6470710 GameState.DebugPrintPower() -         tag=CONTROLLER value=1
+D 02:59:14.6472580 GameState.DebugPrintPower() -         tag=ENTITY_ID value=2
+D 02:59:14.6476340 GameState.DebugPrintPower() -         tag=CARDTYPE value=PLAYER
+D 02:59:14.6481950 GameState.DebugPrintPower() -     Player EntityID=3 PlayerID=2 GameAccountId=[hi=3 lo=2]
+D 02:59:14.6483770 GameState.DebugPrintPower() -         tag=PLAYSTATE value=PLAYING
+D 02:59:14.6485530 GameState.DebugPrintPower() -         tag=CURRENT_PLAYER value=1
+D 02:59:14.6486970 GameState.DebugPrintPower() -         tag=FIRST_PLAYER value=1
+D 02:59:14.6492590 GameState.DebugPrintPower() -         tag=PLAYER_ID value=2
+D 02:59:14.6493880 GameState.DebugPrintPower() -         tag=TEAM_ID value=2
+D 02:59:14.6495200 GameState.DebugPrintPower() -         tag=ZONE value=PLAY
+D 02:59:14.6496470 GameState.DebugPrintPower() -         tag=CONTROLLER value=2
+D 02:59:14.6497780 GameState.DebugPrintPower() -         tag=ENTITY_ID value=3
+D 02:59:14.6500380 GameState.DebugPrintPower() -         tag=CARDTYPE value=PLAYER
 """.strip()
 
 
@@ -80,3 +111,45 @@ def test_tag_value_parsing():
 	tag, value = parser.parse_initial_tag("tag=9999998 value=123")
 	assert tag == 9999998
 	assert value == 123
+
+
+def test_game_initialization():
+	parser = LogParser()
+	parser.read(StringIO(INITIAL_GAME))
+	parser.flush()
+
+	assert len(parser.games) == 1
+	game = parser.games[0]
+	assert len(game.entities) == 3
+	assert len(game.players) == 2
+
+	assert game.tags == {
+		GameTag.TURN: 1,
+		GameTag.ZONE: Zone.PLAY,
+		GameTag.ENTITY_ID: 1,
+		GameTag.NEXT_STEP: Step.BEGIN_MULLIGAN,
+		GameTag.CARDTYPE: CardType.GAME,
+		GameTag.STATE: State.RUNNING,
+	}
+
+	assert game.players[0].tags == {
+		GameTag.PLAYSTATE: PlayState.PLAYING,
+		GameTag.PLAYER_ID: 1,
+		GameTag.TEAM_ID: 1,
+		GameTag.ZONE: Zone.PLAY,
+		GameTag.CONTROLLER: 1,
+		GameTag.ENTITY_ID: 2,
+		GameTag.CARDTYPE: CardType.PLAYER,
+	}
+
+	assert game.players[1].tags == {
+		GameTag.PLAYSTATE: PlayState.PLAYING,
+		GameTag.CURRENT_PLAYER: 1,
+		GameTag.FIRST_PLAYER: 1,
+		GameTag.PLAYER_ID: 2,
+		GameTag.TEAM_ID: 2,
+		GameTag.ZONE: Zone.PLAY,
+		GameTag.CONTROLLER: 2,
+		GameTag.ENTITY_ID: 3,
+		GameTag.CARDTYPE: CardType.PLAYER,
+	}
