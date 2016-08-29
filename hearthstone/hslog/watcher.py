057531679230ae40c39d068c228a8a3e3a066964
Jerome Leclanche <jerome@leclan.ch>
2016-08-30 01:09:33 +0300

    hslog: Implement on_choice and on_choice_sent watcher methods

--- hearthstone/hslog/watcher.py
+++ hearthstone/hslog/watcher.py
@@ -3,6 +3,10 @@ from .parser import LogParser
 
 
 class LogWatcher(LogParser):
+	def __init__(self):
+		super().__init__()
+		self.choices = {}
+
 	def on_entity_update(self, entity):
 		pass
 
@@ -24,6 +28,13 @@ class LogWatcher(LogParser):
 	def on_game_ready(self, game, *players):
 		pass
 
+	def on_choice(self, player, choice):
+		assert choice.id
+		self.choices[choice.id] = choice
+
+	def on_choice_sent(self, player, choice):
+		pass
+
 	def _register_choices(self, *args):
 		packet = super()._register_choices(*args)
 		if packet.type == ChoiceType.MULLIGAN:
@@ -38,6 +49,12 @@ class LogWatcher(LogParser):
 			self.on_entity_update(self._entity_node)
 		if self._metadata_node:
 			self.on_metadata(self._metadata_node)
+		if self._choice_packet:
+			self.on_choice(self._choice_packet.player, self._choice_packet)
+		if self._send_choice_packet:
+			self.on_choice_sent(self.current_game.current_player, self._send_choice_packet)
+		if self._chosen_packet:
+			self.on_choice_sent(self._chosen_packet.entity, self._chosen_packet)
 		super().flush()
 
 	def block_end(self, ts):
