d39559e6b1d4da9667c15e7cbdfe36bcab4ba54e
Jerome Leclanche <jerome@leclan.ch>
2017-01-13 07:12:55 +0200

    setup.py: Add 3.6 classifier

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ CLASSIFIERS = [
 	"Programming Language :: Python :: 3",
 	"Programming Language :: Python :: 3.4",
 	"Programming Language :: Python :: 3.5",
+	"Programming Language :: Python :: 3.6",
 	"Topic :: Games/Entertainment :: Simulation",
 ]
 
