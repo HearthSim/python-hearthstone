9fb545b3142fa859b3563f4416d96c778cd53074
Jerome Leclanche <jerome@leclan.ch>
2017-03-07 13:25:43 +0200

    setup: Move most metadata to setup.cfg

--- hearthstone/__init__.py
+++ hearthstone/__init__.py
@@ -1,3 +1,1 @@
-__author__ = "Jerome Leclanche"
-__email__ = "jerome@hearthsim.net"
 __version__ = "3.4.17720"
--- setup.py
+++ setup.py
@@ -1,33 +1,10 @@
 #!/usr/bin/env python
 
 import hearthstone
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-CLASSIFIERS = [
-	"Development Status :: 5 - Production/Stable",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: MIT License",
-	"Programming Language :: Python",
-	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.4",
-	"Programming Language :: Python :: 3.5",
-	"Programming Language :: Python :: 3.6",
-	"Topic :: Games/Entertainment :: Simulation",
-]
 
 setup(
-	name="hearthstone",
 	version=hearthstone.__version__,
-	packages=find_packages(),
 	package_data={"": ["CardDefs.xml", "Strings/*/*.txt"]},
-	include_package_data=True,
-	author=hearthstone.__author__,
-	author_email=hearthstone.__email__,
-	description="CardDefs.xml parser and Hearthstone enums for Python applications",
-	classifiers=CLASSIFIERS,
-	download_url="https://github.com/HearthSim/python-hearthstone/tarball/master",
-	license="MIT",
-	url="https://github.com/HearthSim/python-hearthstone",
-	zip_safe=True,
 )
