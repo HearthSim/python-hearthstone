c0ce85e9da18a3fe5345ebf759b02de8df9a2890
Jerome Leclanche <jerome@leclan.ch>
2018-04-10 20:41:44 +0300

    enums: Update enums to 23966

--- hearthstone/utils.py
+++ hearthstone/utils.py
@@ -64,7 +64,7 @@ STANDARD_SETS = {
 	],
 	ZodiacYear.RAVEN: [
 		CardSet.CORE, CardSet.EXPERT1,
-		CardSet.UNGORO, CardSet.ICECROWN, CardSet.LOOTAPALOOZA,  # CardSet.GILNEAS
+		CardSet.UNGORO, CardSet.ICECROWN, CardSet.LOOTAPALOOZA, CardSet.GILNEAS,
 	]
 }
 
@@ -73,7 +73,7 @@ ZODIAC_ROTATION_DATES = {
 	ZodiacYear.PRE_STANDARD: datetime.fromtimestamp(0),
 	ZodiacYear.KRAKEN: datetime(2016, 4, 26),
 	ZodiacYear.MAMMOTH: datetime(2017, 4, 7),
-	ZodiacYear.RAVEN: datetime(2018, 4, 19),  # Estimated
+	ZodiacYear.RAVEN: datetime(2018, 4, 12),
 }
 
 
