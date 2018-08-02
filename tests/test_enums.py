4191ec35a16222326a6bdaa2d0ac6cd76a27fef8
Julian Graham <julian@hearthsim.net>
2018-08-02 16:39:36 -0400

    Add test for change to WHIZBANG.is_playable

--- tests/test_enums.py
+++ tests/test_enums.py
@@ -12,6 +12,27 @@ def test_zodiac_dates():
 	assert enums.ZodiacYear.as_of_date(datetime(2018, 5, 1)) == enums.ZodiacYear.RAVEN
 
 
+def test_cardclass():
+	playable_cards = [
+		enums.CardClass.DRUID,
+		enums.CardClass.HUNTER,
+		enums.CardClass.MAGE,
+		enums.CardClass.PALADIN,
+		enums.CardClass.PRIEST,
+		enums.CardClass.ROGUE,
+		enums.CardClass.SHAMAN,
+		enums.CardClass.WARLOCK,
+		enums.CardClass.WARRIOR
+	]
+
+	for c in playable_cards:
+		assert c.is_playable
+
+	for c in enums.CardClass:
+		if c not in playable_cards:
+			assert not c.is_playable
+
+
 def test_gametype():
 	gt = enums.GameType
 	bgt = enums.BnetGameType
