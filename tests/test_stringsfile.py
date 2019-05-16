506a96716f8da37874c78496b64dfe745f597a86
Benedict Etzel <benedict@hearthsim.net>
2019-05-16 14:15:26 -0700

    Flake8 fixes

--- tests/test_stringsfile.py
+++ tests/test_stringsfile.py
@@ -2,12 +2,13 @@ from io import StringIO
 
 from hearthstone.stringsfile import load
 
+
 TEST_STRINGS = """TAG	TEXT	COMMENT	AUDIOFILE
 VO_ICC09_Saurfang_Male_Orc_CursedBlade_01	Who’s idea was this?
 VO_ICC09_Saurfang_Male_Orc_Doomerang_01	Hmm… I gotta get one of those…
 
 VO_ICC06_Marrowgar_Male_BoneWraith_Intro_01	None may enter the master's sanctum! 
-VO_ICC06_Marrowgar_Male_BoneWraith_Bonespike_01	The only escape is death!"""
+VO_ICC06_Marrowgar_Male_BoneWraith_Bonespike_01	The only escape is death!"""  # noqa: W291
 
 
 def test_load_blank_line():
