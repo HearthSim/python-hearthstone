f679fcec3b2d64a5a35103906830dc99c5476a3f
Julian Graham <julian@hearthsim.net>
2020-01-28 11:06:48 -0500

    Add YEAR_OF_THE_DRAGON to the, er, year of the dragon zodiac year

--- hearthstone/utils.py
+++ hearthstone/utils.py
@@ -71,7 +71,7 @@ STANDARD_SETS = {
 	ZodiacYear.DRAGON: [
 		CardSet.CORE, CardSet.EXPERT1,
 		CardSet.GILNEAS, CardSet.BOOMSDAY, CardSet.TROLL, CardSet.DALARAN, CardSet.ULDUM,
-		CardSet.WILD_EVENT, CardSet.DRAGONS
+		CardSet.WILD_EVENT, CardSet.DRAGONS, CardSet.YEAR_OF_THE_DRAGON
 	]
 }
 
