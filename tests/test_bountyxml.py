24341ee3dabcbad751c849ef8007b669bdce5141
Julian Graham <julian@hearthsim.net>
2022-02-15 15:31:08 -0500

    Fix broken Mercenaries bountyxml test

--- tests/test_bountyxml.py
+++ tests/test_bountyxml.py
@@ -6,5 +6,5 @@ def test_bountyxml_load():
 
 	assert bounty_db
 
-	assert bounty_db[47].boss_name == "Cap'n Hogger"
+	assert bounty_db[68].boss_name == "The Anointed Blades"
 	assert bounty_db[58].region_name == "The Barrens"
